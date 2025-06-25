1cd0ea904f8204ab134cfe7c9106b8657e24e0ae
iwa <iwaongit@gmail.com>
2025-06-25 15:14:08 +0100

    feat: add simple http api example

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use warp::{Filter, Reply};
+use serde::Serialize;
+use std::convert::Infallible;
+
+#[derive(Serialize)]
+struct ApiResponse<'a> {
+    message: &'a str,
+    data: &'a str,
+}
+
+async fn create_response<'a>(message: &'a str, data: &'a str) -> Result<impl Reply + use<>, Infallible> {
+    let response = ApiResponse { message, data };
+    Ok(warp::reply::json(&response))
+}
+
+#[tokio::main]
+async fn main() {
+    let api_route = warp::path!("api" / "data" / String)
+        .and_then(|data: String| async move {
+            let message = "Hello from the API!";
+            create_response(message, &data).await
+        });
+
+    warp::serve(api_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
